84ec900f497ba8ecaed4e3c286500ecc6a276683
Prashant Thapa <rusarai123@gmail.com>
2024-03-08 22:03:03 +0545

    first commit

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,12 @@
+import {Hero,FeaturedProducts, Testimonials, Faq} from "./components"
+
+export const HomePage = () => {
+    return (
+      <main>     
+            <Hero/>
+            <FeaturedProducts/>
+            <Testimonials/>
+            <Faq/>
+      </main>
+    )
+  }
---
+++ src/pages/Home/components/index.js
@@ -0,0 +1,4 @@
+export { Hero } from "./Hero";
+export { FeaturedProducts } from "./FeaturedProducts"
+export { Testimonials } from "./Testiomonials"
+export {Faq} from "./Faq"
